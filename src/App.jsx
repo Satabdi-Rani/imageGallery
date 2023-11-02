dcad4e03a35f60393d67b567a7a54af6a790c38c
Satabdi <satabdiraniweb@gmail.com>
2023-11-02 20:53:48 +0600

    Initialization

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-7xl font-bold underline">
+      Hello world! bye
+    </h1>
+    </>
+  )
+}
+
+export default App
